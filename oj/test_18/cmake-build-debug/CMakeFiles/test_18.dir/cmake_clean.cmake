ced2ab258163e321eacfbbec02145f0401b4645e
xu1feng <2465549609@qq.com>
2022-11-05 21:21:06 +0800

    update

---
+++ oj/test_18/cmake-build-debug/CMakeFiles/test_18.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_18.dir/main.cpp.obj"
+  "libtest_18.dll.a"
+  "test_18.exe"
+  "test_18.exe.manifest"
+  "test_18.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_18.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
