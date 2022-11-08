6f81b7b9acb3a77d97b57011fcf9265923d41ca2
xu1feng <2465549609@qq.com>
2022-11-08 22:04:18 +0800

    update

---
+++ oj/test_19/cmake-build-debug/CMakeFiles/test_19.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_19.dir/main.cpp.obj"
+  "libtest_19.dll.a"
+  "test_19.exe"
+  "test_19.exe.manifest"
+  "test_19.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_19.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
